07219c9cdbad56d84a1f3b0719545ce34fe6c5bd
Daddy2054 <stivali-tinte-0w@icloud.com>
2025-04-22 19:04:33 +0200

    Initial Commit

---
+++ gardenapp/gardenappApp.swift
@@ -0,0 +1,17 @@
+//
+//  gardenappApp.swift
+//  gardenapp
+//
+//  Created by Jean on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct gardenappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
